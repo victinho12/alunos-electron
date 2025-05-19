4e4e1230052e1cddae2aa89c9fcbd10d7c7144eb
Victor <victoreduardomartins2@gmail.com>
2025-05-19 19:51:23 -0300

    Atualizando dados
    Criando a atualizando projeto do senac

---
+++ db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',         // <- substitua pelo seu usuário do PostgreSQL
+  host: 'localhost',
+  database: 'senac',       // <- substitua pelo nome do seu banco de dados
+  password: '123456',       // <- substitua pela senha correta
+  port: 5432,                  // porta padrão do PostgreSQL
+});
+
+
+function query(text, params){
+    return pool.query(text, params)
+}
+
+module.exports = {
+  query 
+  }
+;
